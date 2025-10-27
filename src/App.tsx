387543c5bda6260a64ec45fcbe40a830d6dc8b56
Jeison Redondo <jeida16@yahoo.es>
2025-10-27 17:44:05 -0500

    [Git init]Starting the proyect

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>FastPort</h1>
+      <p>Tu portafolio de productos, facil y rapido</p>
+    </>
+  )
+}
+
+export default App

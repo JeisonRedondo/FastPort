32ef0cf3f6efe3ab79877f3bd2256e0dd29be519
Jeison Redondo <jeida16@yahoo.es>
2026-01-23 06:56:19 -0500

    [Adding]Adding icons for dashboard

--- src/components/Dashboard/Sidebar.tsx
+++ src/components/Dashboard/Sidebar.tsx
@@ -1,13 +1,34 @@
+import { Keyboard } from "phosphor-react";
+
 function Sidebar() {
   return (
     <aside className="sidebar">
       <h2>FastPort</h2>
       <nav>
-        <ul>
-          <li>Inicio</li>
-          <li>Proyectos</li>
-          <li>Configuración</li>
-        </ul>
+        <section>
+          <article>
+            <h4>Dashboard</h4>
+            <Keyboard size={32} weight="fill"/>
+          </article>
+          <article>
+            <h4>Mi portafolio</h4>
+          </article>
+          <article>
+            <h4>Historial de Catalogos</h4>
+          </article>
+          <article>
+            <h4>Blog</h4>
+          </article>
+          <article>
+            <h4>Precios</h4>
+          </article>
+          <article>
+            <h4>Soporte</h4>
+          </article>
+          <article>
+            <h4>Configuración</h4>
+          </article>
+        </section>
       </nav>
     </aside>
   );
